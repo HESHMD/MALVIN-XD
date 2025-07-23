481e7ae75c420d57fc388414977a05b9aec97907
HESHMD <kavishkaheshan546@gmail.com>
2025-07-23 13:02:00 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~Zz0mFIYa#kDR3_C8HietPNvBeDQfZsufEUg5u0GKnvG95ouQ_Ybg",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
